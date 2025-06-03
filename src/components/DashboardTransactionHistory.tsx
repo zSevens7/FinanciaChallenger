a24c85b4b60c04013da6362d304679d9434d6753
Rayan Morais <rayan_cm2021@icloud.com>
2025-06-02 23:49:52 -0300

    inicia layout dashboard

---
+++ src/components/DashboardTransactionHistory.tsx
@@ -0,0 +1,9 @@
+const DashHistory = () => {
+  return (
+    <div className="flex flex-col gap-4">
+      <h2 className="text-2xl font-bold">Transaction History</h2>
+      <p className="text-gray-600">No transactions found.</p>
+    </div>
+  );
+}
+export default DashHistory;
