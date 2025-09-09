7144c8685ebe46ae0e8d14b7d01fb658c0185568
zSevens7 <gabrielteperino@yahoo.com.br>
2025-09-09 18:07:38 -0300

    atualizando o backend

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -12,7 +12,7 @@ const router = express.Router();
 const db = mysql.createPool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
   waitForConnections: true,
   connectionLimit: 10,
