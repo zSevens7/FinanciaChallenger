8d9685620bac86557509569175eeedec14a40b5c
zSevens7 <gabrielteperino@yahoo.com.br>
2025-09-09 18:15:57 -0300

    atualizando o backend

--- backend/src/routes/auth.js
+++ backend/src/routes/auth.js
@@ -10,7 +10,7 @@ const router = express.Router();
 
 // Conexão MySQL remota
 const db = mysql.createPool({
-  host: process.env.DB_HOST,
+  host: "31.97.64.21", // força IPv4
   user: process.env.DB_USER,
   password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
