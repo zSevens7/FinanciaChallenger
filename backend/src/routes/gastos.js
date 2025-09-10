ad211e7929c2beaa957fbf1bd8a4589478a9b2a1
zSevens7 <gabrielteperino@yahoo.com.br>
2025-09-10 12:56:54 -0300

    atualizando o backend 2

--- backend/src/routes/gastos.js
+++ backend/src/routes/gastos.js
@@ -9,7 +9,7 @@ const router = express.Router();
 const dbConfig = {
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
-  password: process.env.DB_PASS,
+  password: process.env.DB_PASSWORD,
   database: process.env.DB_NAME,
 };
 
